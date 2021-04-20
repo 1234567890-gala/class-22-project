7f1cd2ae93f99dad1cb7a4e678a890308a459f77
1234567890-gala <80503681+1234567890-gala@users.noreply.github.com>
2021-04-20 13:47:36 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,7 +1,7 @@
-var starImg, fairyImg, bgImg;
-var fairy , fairyVoice;
-var star, starBody;
 
+
+var helicopterIMG, helicopterSprite, packageSprite,packageIMG;
+var packageBody,ground
 const Engine = Matter.Engine;
 const World = Matter.World;
 const Bodies = Matter.Bodies;
@@ -9,57 +9,91 @@ const Body = Matter.Body;
 
 function preload()
 {
-	starImg = loadImage("images/star.png");
-	fairyImg = loadAnimation("images/fairyImage1.png","images/fairyImage2.png");
-	bgImg = loadImage("images/starNight.png");
-	fairyVoice = loadSound("sound/JoyMusic.mp3");
-
+	helicopterIMG=loadImage("helicopter.png")
+	packageIMG=loadImage("package.png")
 }
 
 function setup() {
-	createCanvas(800, 750);
+	createCanvas(800, 700);
+	rectMode(CENTER);
+	
 
-	// fairyVoice.play();
+	packageSprite=createSprite(width/2, 80, 10,10);
+	packageSprite.addImage(packageIMG)
+	packageSprite.scale=0.2
 
-	fairy = createSprite(130, 520);
-	fairy.addAnimation("fairyflying",fairyImg);  
-	fairy.scale =0.25;
+	helicopterSprite=createSprite(width/2, 200, 10,10);
+	helicopterSprite.addImage(helicopterIMG)
+	helicopterSprite.scale=0.6
 
-	star = createSprite(650,30);
-	star.addImage(starImg);
-	star.scale = 0.2;
+	groundSprite=createSprite(width/2, height-35, width,10);
+	groundSprite.shapeColor=color(255)
+
 
 	engine = Engine.create();
 	world = engine.world;
 
-	starBody = Bodies.circle(650 , 30 , 5 , {restitution:0.5, isStatic:true});
-	World.add(world, starBody);
+	packageBody = Bodies.circle(width/2 , 200 , 5 , {restitution:0.4, isStatic:true});
+	World.add(world, packageBody);
 	
-	Engine.run(engine);
 
+	//Create a Ground
+	ground = Bodies.rectangle(width/2, 650, width, 10 , {isStatic:true} );
+ 	World.add(world, ground);
+
+ 	boxPosition=width/2-100
+ 	boxY=610;
+
+
+ 	boxleftSprite=createSprite(boxPosition, boxY, 20,100);
+ 	boxleftSprite.shapeColor=color(255,0,0);
+
+ 	boxLeftBody = Bodies.rectangle(boxPosition+20, boxY, 20,100 , {isStatic:true} );
+ 	World.add(world, boxLeftBody);
+
+ 	boxBase=createSprite(boxPosition+100, boxY+40, 200,20);
+ 	boxBase.shapeColor=color(255,0,0);
+
+ 	boxBottomBody = Bodies.rectangle(boxPosition+100, boxY+45-20, 200,20 , {isStatic:true} );
+ 	World.add(world, boxBottomBody);
+
+ 	boxleftSprite=createSprite(boxPosition+200 , boxY, 20,100);
+ 	boxleftSprite.shapeColor=color(255,0,0);
+
+ 	boxRightBody = Bodies.rectangle(boxPosition+200-20 , boxY, 20,100 , {isStatic:true} );
+ 	World.add(world, boxRightBody);
+
+
+	Engine.run(engine);
+  
 }
 
 
 function draw() {
-  background(bgImg);
+  rectMode(CENTER);
+  background(0);
+ 
+  packageSprite.x= packageBody.position.x 
+  packageSprite.y= packageBody.position.y 
 
+  packageSprite.x = helicopterSprite.y;
+  packageSprite.x = helicopterSprite.x;
+
+  
   drawSprites();
-
 }
+function keyPressed(){
+	if(keyCode === RIGHT_ARROW){ 
+		helicopterSprite.x = helicopterSprite.x + 20; 
+	   } 
+		if(keyCode === LEFT_ARROW){
+helicopterSprite.x = helicopterSprite.x - 20; 
+	   }
+	   if(keyCode === DOWN_ARROW){
+		Matter.Body.setStatic(packageBody, false);
+	   }
+		
+	  }
 
-function keyPressed() {
-	//write code here
-	 if(keyCode === RIGHT_ARROW){ 
-		 fairy.x = fairy.x + 20; 
-		} 
-		 if(keyCode === LEFT_ARROW)
-		 { fairy.x = fairy.x - 20; 
-		}
-		 if (keyCode === DOWN_ARROW) 
-		{ Matter.Body.setStatic(starBody,false); 
-		} 
-		if(keyCode === DOWN_ARROW){ 
-			star.y = star.y + 500; 
-		   } 
-}
+
 
